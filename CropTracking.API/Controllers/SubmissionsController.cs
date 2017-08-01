663ca0f8ab347581e030507a58ecb065696f0417
Cindy Kee <Cindy Kee>
2017-07-31 17:23:48 -0700

    Add project files.

---
+++ CropTracking.API/Controllers/SubmissionsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+ 
+namespace CropTracking.API.Controllers
+{
+    [Route("api/[controller]")]
+    public class SubmissionsController : Controller
+    {
+        [HttpGet()]
+        public IActionResult GetCities()
+        {
+            return Ok(CitiesDataStore.Current.Submissions);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetCity(int id)
+        {
+            var city = CitiesDataStore.Current.Submissions.FirstOrDefault(c => c.Id == id);
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(city);
+        }
+    }
+}
