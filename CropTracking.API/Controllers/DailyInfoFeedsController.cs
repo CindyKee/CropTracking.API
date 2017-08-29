e20205970a9b64e827d3f1097403f6ed1359ce0a
Robert Kee <robert@thekees.com>
2017-08-29 13:33:50 -0700

    yolo

--- CropTracking.API/Controllers/DailyInfoFeedsController.cs
+++ CropTracking.API/Controllers/DailyInfoFeedsController.cs
@@ -4,16 +4,19 @@ using System.Linq;
 
 namespace CropTracking.API.Controllers
 {
+
+    [Route("api/[controller]")]
+
     public class DailyInfoFeedsController : Controller
     {
-        // TODO 1: Add the Route attribute to this class above (see other controllers for how)
-        // TODO 2: Write a standard Get method that returns all DailyInformationFeeds records 
-        //  (I added it to the data store already)
-        // TODO 3: Add sorting to the retrieval of data. Use .OrderBy().ThenBy() on the DailyInformationFeeds
-        //  property. You might have to look up examples on what to put in the parentheses of both OrderBy 
-        //  and ThenBy. We want to order by CompanyName and then by ReportDate.
-
-        // TODO 4: Time to go back and start watching Module 4 of the Pluralsight course. Need to see how 
-        //  to do creates and updates through the Web API.
+        [HttpGet()]
+        public IActionResult GetDailyInformationFeeds()
+        {
+            return Ok(CropDataStore.Current.DailyInformationFeeds
+                .OrderBy(sorted => sorted.CompanyName)
+                .ThenBy(sorted => sorted.ReportDate));
+            // TODO 4: Time to go back and start watching Module 4 of the Pluralsight course. Need to see how 
+            //  to do creates and updates through the Web API.
+        }
     }
 }
